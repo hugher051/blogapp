3b3bdfd1f6c11c53a0729e4293ff174f2c59359a
Hugher Barja Carhuaz <hugherbarja@gmail.com>
2021-06-28 23:50:46 -0500

    commit 1.2

--- boostrap_blog/boostrap_blog/settings.py
+++ boostrap_blog/boostrap_blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-a+8#dfn7^m&xk)-gj55$o4zfk6$1jzrw^sc(tk8-t6^&=)3wz$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
